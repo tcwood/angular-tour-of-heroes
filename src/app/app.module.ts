2cb764867ea4fa4f169357efab9e5abebb0b896a
Travis Wood <travis_cw@yahoo.com>
2017-03-24 17:32:18 -0700

    Add dashboard component that shows four heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,38 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { HeroesComponent } from './heroes.component'
+import { DashboardComponent } from './dashboard.component'
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
+
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+    ])
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
     HeroesComponent
   ],
