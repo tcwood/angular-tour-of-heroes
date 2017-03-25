820ace7f2a35f9f0ab162f95521f5154f73e09ed
Travis Wood <travis_cw@yahoo.com>
2017-03-24 17:51:23 -0700

    Modify how hero detail is selected. Provide back button

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -37,6 +37,10 @@ AppModule = __decorate([
                 {
                     path: 'dashboard',
                     component: dashboard_component_1.DashboardComponent
+                },
+                {
+                    path: 'detail/:id',
+                    component: hero_detail_component_1.HeroDetailComponent
                 }
             ])
         ],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,10 @@ import { HeroService } from './hero.service';
       {
         path: 'dashboard',
         component: DashboardComponent
+      },
+      {
+        path: 'detail/:id',
+        component: HeroDetailComponent
       }
     ])
   ],
